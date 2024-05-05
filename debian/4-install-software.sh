d647d8c3a2122b9365b8ae28a5ce0e097cc7227a
龙茶清欢 <2320391937@qq.com>
2024-05-05 13:56:02 +0800

    backup

--- debian/4-install-software.sh
+++ debian/4-install-software.sh
@@ -7,21 +7,13 @@ set -e
 # 读取用户密码
 read -p "请输入你的登陆密码：" password
 
-# 用于浏览和安装 GNOME Shell 扩展的实用程序。
-flatpak install flathub com.mattjakeman.ExtensionManager -y
 # 可让您一键保存当前配置（主题、图标、壁纸、所有桌面环境设置、扩展等）
 flatpak install flathub io.github.vikdevelop.SaveDesktop -y
-# U盘镜像刻录
-flatpak install flathub com.system76.Popsicle -y
 # 聊天、通讯
 flatpak install flathub com.qq.QQ -y
 flatpak install flathub com.tencent.WeChat -y
-# 腾讯会议
-flatpak install flathub com.tencent.wemeet -y
 # YesPlayMusic音乐
 flatpak install flathub io.github.qier222.YesPlayMusic -y
-# Chrome浏览器
-flatpak install flathub com.google.Chrome -y
 # 百度网盘
 flatpak install flathub com.baidu.NetDisk -y
 # Flatseal 是一个图形实用程序，用于查看和修改 Flatpak 应用程序的权限。
@@ -29,8 +21,6 @@ flatpak install flathub com.github.tchx84.Flatseal -y
 # 以上软件是基本必装的常用软件
 # Obsidian 文本编辑器
 flatpak install flathub md.obsidian.Obsidian -y
-# 雷鸟邮箱
-flatpak install flathub org.mozilla.Thunderbird -y
 
 # 下载安装非软件商城编程软件
 cd $HOME/下载
@@ -38,7 +28,7 @@ echo "==========开始安装 jetbrains-toolbox 代码编辑器管理工具======
 # -O 选项指示curl将下载的文件保存为与远程文件相同的文件名，为大写字母
 # 使用`-OJ`选项参数后，`curl`会从URL中提取文件名，并将其用作保存文件的名称。
 # toolbox
-curl -OJ https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.3.1.31116.tar.gz
+wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-2.3.1.31116.tar.gz
 # 解压缩 .tar.gz 文件
 tar -xzvf jetbrains-toolbox-2.3.1.31116.tar.gz
 # 重命名
@@ -50,7 +40,7 @@ echo $password | su -c "mv ToolBox /opt" -s /bin/bash root
 
 echo "==========开始安装 Apifox 接口测试工具=========="
 # Apifox
-curl -OJ https://file-assets.apifox.com/download/Apifox-linux-deb-latest.zip
+wget https://file-assets.apifox.com/download/Apifox-linux-deb-latest.zip
 # 解压 ZIP 压缩文件
 unzip Apifox-linux-deb-latest.zip
 cd Apifox-linux-deb-latest
