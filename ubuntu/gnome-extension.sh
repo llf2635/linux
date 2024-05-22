0e3c3f341d2b9389804291cf69b37108f19ca943
龙茶清欢 <2320391937@qq.com>
2024-05-23 00:35:47 +0800

    backup

--- ubuntu/gnome-extension.sh
+++ ubuntu/gnome-extension.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+# gnome-extensions 直接使用可以查看扩展的所有命令的作用
+# help      打印帮助
+# version   打印版本
+# enable    启用扩展
+# disable   禁用扩展
+# reset     重置扩展
+# uninstall 卸载扩展
+# list      列出扩展
+# info      显示扩展信息
+# show      显示扩展信息
+# prefs     打开扩展首选项
+# create    创建扩展
+# pack      打包扩展
+# install   安装扩展包
+# gnome-extensions help
 # gnome-extensions list	列出可安装的扩展插件
-
 gnome-extensions install rounded-window-corners@yilozt.shell-extension.zip
+
+
+user-theme@gnome-shell-extensions.gcampax.github.com
+compiz-alike-magic-lamp-effect@hermes83.github.com
+blur-my-shell@aunetx
+hidetopbar@mathieu.bidon.ca
+just-perfection-desktop@just-perfection
+bluetooth-quick-connect@bjarosze.gmail.com
+status-area-horizontal-spacing@mathematical.coffee.gmail.com
+caffeine@patapon.info
+nightthemeswitcher@romainvigier.fr
+kimpanel@kde.org
+dash-to-dock@micxgx.gmail.com
+gtk4-ding@smedius.gitlab.com
+drive-menu@gnome-shell-extensions.gcampax.github.com
